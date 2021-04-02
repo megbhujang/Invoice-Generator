1e742780b7de355d72418ec96888ce6a3638c85d
Megana Bhujang <megbhujang@gmail.com>
2021-04-02 19:44:30 +0530

    First Commit

---
+++ templates/db_invoice_generator.py
@@ -0,0 +1,42 @@
+import mysql.connector
+mydb = mysql.connector.connect(host="localhost", user="root", password="", database="invoice")
+print(mydb)
+
+if(mydb):
+    print("Connection successful")
+else:
+    print("Connection importunsuccessful")
+
+mycursor = mydb.cursor()
+mycursor.execute("Show tables")
+
+for db in mycursor:
+    print(db)
+
+mycursor.execute("SELECT * FROM trainer_deatils")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+mycursor.execute("SELECT * FROM college_details")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+mycursor.execute("SELECT * FROM training_details")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+
+mycursor.execute("SELECT * FROM bank_details")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
